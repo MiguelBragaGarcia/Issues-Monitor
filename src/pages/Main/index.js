329e6936a78463b93c45c69e5b3bd952b2c4310d
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-02-18 10:24:23 -0300

    Adiciona filtros de entrada de dados

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -12,6 +12,7 @@ export default class Main extends Component {
         newRepo: '',
         repositories: [],
         loading: false,
+        error: null,
     };
 
     // Carregar os dados do localStorage
@@ -40,24 +41,38 @@ export default class Main extends Component {
 
     handleSubmit = async e => {
         e.preventDefault();
-        this.setState({ loading: true });
+        this.setState({ loading: true, error: false });
+        try {
+            const { newRepo, repositories } = this.state;
+            if (newRepo === '') {
+                const error = 'Você precisa adicionar um repositório'; // O throw seguindo boas práticas pode ser feito dessas maneiras com ou sem o objeto error
+                throw error;
+            }
 
-        const { newRepo, repositories } = this.state;
-        const response = await api.get(`/repos/${newRepo}`);
+            const hasRepo = await repositories.find(f => f.name === newRepo); // Busca nos repositórios se já existe o repositório que foi requisitado
+            if (hasRepo) {
+                throw new Error('Repositório duplicado'); // O throw seguindo boas práticas pode ser feito dessas maneiras com ou sem o objeto error
+            }
 
-        const data = {
-            name: response.data.full_name,
-        };
+            const response = await api.get(`/repos/${newRepo}`);
+            const data = {
+                name: response.data.full_name,
+            };
 
-        this.setState({
-            repositories: [...repositories, data],
-            newRepo: '',
-            loading: false,
-        });
+            this.setState({
+                repositories: [...repositories, data],
+                newRepo: '',
+                loading: false,
+            });
+        } catch (error) {
+            this.setState({ error: true }); // Mostra que deu erro
+        } finally {
+            this.setState({ loading: false }); // Libera o botão do aplicativo
+        }
     };
 
     render() {
-        const { newRepo, repositories, loading } = this.state;
+        const { newRepo, repositories, loading, error } = this.state;
         return (
             <Container>
                 <h1>
@@ -65,9 +80,8 @@ export default class Main extends Component {
                     Repositórios
                 </h1>
 
-                <Form onSubmit={this.handleSubmit}>
+                <Form error={error} onSubmit={this.handleSubmit}>
                     <input
-                        required
                         type="text"
                         placeholder="Adiconar repositório"
                         value={newRepo}
